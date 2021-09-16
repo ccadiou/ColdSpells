b9a794da9a6c6f9ab8d079920533267b49d6bedd
Camille Cadiou <camille.cadiou@lsce.ipsl.fr>
2021-09-16 18:30:41 +0200

    Debut du code pour calcul de l'analogue weather generator

--- script_main.R
+++ script_main.R
@@ -1,3 +1,17 @@
-#test
 library(ggplot2)
-test <- 12
+
+########## INPUT UTILISATEUR ##########
+date0 <- as.Date("20101201", format = "%Y%m%d")
+
+
+
+########## CHARGEMENT ET FORMATAGE DES DONNÉES ##########
+#Chargement des données
+data_ana <- read.table("../../Data/Winter/ana_z500_1_-20.30.30.70.txt",header=TRUE)
+#Conversion des colonnes dates en format date
+data_ana[, cols <- grep("^date", names(data_ana))] <- lapply(data_ana[, cols <- grep("^date", names(data_ana))],
+                                                             function(col) as.Date(as.character(col), format = "%Y%m%d"))
+
+
+
+
