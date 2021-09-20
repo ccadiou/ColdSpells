3b4ee02dcda0eefd6d50f4e96c9766fb7a1f70c1
Camille Cadiou <camille.cadiou@lsce.ipsl.fr>
2021-09-20 17:55:52 +0200

    Réorganisation du projet et script permettant de faire les séries temp de T&P

---
+++ fct_plot.R
@@ -0,0 +1,31 @@
+plot_serie_temp <- function(df,title="",xlegend="",ylegend=""){
+  p <- ggplot(df,aes(x=df[,1],y=df[,2]))
+  return(
+    p + geom_line()+
+    geom_point(shape = 19, aes(color="")) +
+      scale_color_manual(values = c("red")) +
+      labs(
+        titles = title,
+        x = xlegend,y = ylegend
+      ) +
+      theme_classic()+
+      theme(legend.position = "None",legend.title = element_blank())
+  )
+}
+
+plot_2y <- function(df1,df2,factor,title="",xlegend="",ylegend1="",ylegend2=""){
+  p <- ggplot()
+  return(
+    p + geom_line(data=df1, aes(x=df1[,1], y=df1[,2]), color="red") +
+      geom_line(data=df2, aes(x=df2[,1], y=df2[,2]/factor), color="blue")+
+      scale_y_continuous(limits=c(0, 8), 
+                         sec.axis = sec_axis(~ . *factor, name = ylegend2))+
+      labs(titles = title,x=xlegend, y=ylegend1)+
+      # geom_point(shape = 19, aes(color="")) +
+      scale_color_manual(values = c("red")) +
+      theme_classic()+
+      theme(legend.position = "None",legend.title = element_blank())
+  )
+}
+
+
---
+++ script_serie_temp.R
@@ -0,0 +1,29 @@
+library(ncdf4)
+library(ggplot2)
+library(lubridate)
+
+source("fct_plot.R")
+source("fct_ncdf.R")
+
+###### Load data ######
+path <- "~/Documents/These/Data/Winter/"
+#Total precipitation data
+name_tp <- "era5_tp_DJF_fr_mean"
+nc_tp <- getNcFile(path,name_tp)
+
+#Temperature at 2m
+name_t2m <- "era5_t2m_DJF_fr_mean"  
+nc_t2m <- getNcFile(path,name_t2m)
+
+#Time
+t <- c(1950:2021)
+
+df_t2m <- data.frame(cbind(t,as.vector(ncvar_get(nc_t2m,"t2m"-273.15)))) #data frame de la série temporelle et conversion kelvin <-  celsius
+df_tp <- data.frame(cbind(t,as.vector(ncvar_get(nc_tp,"tp"))))
+
+#plot
+plot_serie_temp(df_t2m)
+plot_serie_temp(df_tp)
+
+plot_2y(df_t2m,df_tp,max(df_tp[,2])/max(df_t2m[,2]),#title = "Temperature and precipitation",
+        xlegend = "Time",ylegend1="2m temperature [°C]",ylegend2="Total precipiation [mm]")
