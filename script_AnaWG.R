1f56584a4298d8c389c51dfab0116ade66e84dbb
Camille Cadiou <camille.cadiou@lsce.ipsl.fr>
2021-11-02 17:35:18 +0100

    Adaptation du script d'analyse de l'AnaWG à la sortie du script HWgen_AnImSa.R

--- script_AnaWG.R
+++ script_AnaWG.R
@@ -1,5 +1,6 @@
 library(ggplot2)
 library(dplyr)
+library(plyr)
 
 source("fct_plot.R")
 
@@ -9,10 +10,13 @@ load("./data/t2m_daily_fr.RData")
 #Données totla precipitation
 load("./data/tp_daily_fr.RData")
 
-load("../../Data/WEGE_ERA5_1950-2021.RData")
-load("../../Data/Winter/WEGE_ERA5_1950-2021.Rdat")
+# load("../../Data/Winter/WEGE_ERA5_1950-2021.Rdat")
+load("../../Data/Winter/TG-Orly-m12d1_HW-animpsa_cal3_TG0meth1-834413.Rdat")
 
-View(XX.wg)
+# View(simu.dyn)
+simu.dyn.2010 <-  adply(simu.dyn$l.X$`2010`, 1)
+# df  <- do.call(rbind,simu.dyn.2010)
+simu.dyn.2010$t.sim <- as.character(simu.dyn.2010$t.sim)
+simu.dyn.2010$t.sim <- as.Date(simu.dyn.2010$t.sim,format="%Y%m%d")
 
-
-  
+test <- cbind(simu.dyn.2010,"t"=seq(as.Date("2010-12-01"),as.Date("2011-03-01"),by="day"))
