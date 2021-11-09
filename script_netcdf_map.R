7c592470cee2d5c3fe682425950508ec9b1747c8
Camille Cadiou <camille.cadiou@lsce.ipsl.fr>
2021-11-09 18:27:12 +0100

    Amélioration des script de plot pour les cartes de pression et anomalie de pression

--- script_netcdf_map.R
+++ script_netcdf_map.R
@@ -23,15 +23,34 @@ plot_list <- lapply(ncnames,function(f) plot_nc_raster_from_file(ncpath,f,"z500"
 
 plot_nc_raster_from_file(ncpath,"era5_z500_DJFmean.nc","z500",zmin,zmax)
 
-#pour essayer de mettre les titres
-x<-1:10
-par(mar=c(2.5,2.5,1,1))
-layout(matrix(c(1,2,3,4,1,5,3,6),ncol=2),heights=c(1,3,1,3))
-plot.new()
-text(0.5,0.5,"First title",cex=2,font=2)
-plot(x)
-plot.new()
-text(0.5,0.5,"Second title",cex=2,font=2)
-hist(x)
-boxplot(x)
-barplot(x)
+##### Avec les fonctions de pascal _____________________________________________________________________________________________________________________________#####
+library(ncdf4)
+source("fct_plotmap.R")
+# set path and filename
+ncpath <- "~/Documents/These/Data/Winter/submeans_z500_largefield/"
+# ncnames <- list.files(ncpath) 
+ncname <- paste(ncpath,"era5_z500_10_1956-02-09_1956-02-18.nc",sep="")
+dname <- "z500"
+# open a netCDF file
+ncin <- nc_open(ncname)
+lon <- ncvar_get(ncin, "lon")
+lat <- ncvar_get(ncin, "lat", verbose = F)
+nc_array <- ncvar_get(ncin, dname)
+
+ncpath_anomalie <- "~/Documents/These/Data/Winter/submeans_z500_anomalie_largefield/"
+# ncnames <- list.files(ncpath) 
+ncname_anomalie <- paste(ncpath_anomalie,"era5_z500_10_1956-02-09_1956-02-18_anomalie.nc",sep="")
+# open a netCDF file
+ncin_anomalie <- nc_open(ncname_anomalie)
+lon <- ncvar_get(ncin_anomalie, "lon")
+lat <- ncvar_get(ncin_anomalie, "lat", verbose = F)
+nc_array_anomalie <- ncvar_get(ncin_anomalie, dname)
+
+image.cont.ano(lon,lat,nc_array_anomalie,titre="test",transpose = FALSE)
+image.cont.c(lon,lat,nc_array,titre="test",transpose = FALSE,add=TRUE)
+
+
+pretty(nc_array_anomalie,10)
+
+
+
--- script_submeans.R
+++ script_submeans.R
@@ -35,26 +35,27 @@ df_extdate <- rbind(cbind(ndays_min(30,path,var,extreme),"n_days"=30,"index"=nda
                     cbind(ndays_min(10,path,var,extreme),"n_days"=10,"index"=ndays_minidx(10,path,var,extreme)[,2]),
                     cbind(ndays_min(3,path,var,extreme),"n_days"=3,"index"=ndays_minidx(3,path,var,extreme)[,2]),
                     cbind(df_var,"n_days"=90,"index"=as.Date(paste(df_var$date,"-01-01",sep=""),format="%Y-%m-%d")))
-df_extdate_3 <- select_extremes(df_extdate,var,n_days,extreme,3)
+df_extdate_3 <- select_extremes(df_extdate,var,n_days,extreme,10)
 df_extdate_3 <- df_extdate_3[c(10:12,1:3,4:6,7:9),]
 df_extdate_3$n_days <- factor(df_extdate_3$n_days, levels=c(90,30,10,3))
 
 #plot
 # df_extdate_3$date <- factor(df_extdate_3$date,levels=levels(as.factor(df_extdate$date)))
-df_extdate_3$date <- as.numeric(df_extdate_3$date)
+df_extdate_3$date <- as.numeric(as.character(df_extdate_3$date))
 plot_submean(df_extdate_3,xlabel = "Date",ylabel = var_label,legend_title = "Time range\n(days)")
-# save(df_extdate3,file="./data/t2m_DJF_minbyperiodduration")
+ # save(df_extdate3,file="./data/t2m_DJF_minbyperiodduration")
 df_extdate_3$n_days <- as.numeric(as.character(df_extdate_3$n_days))
-write.table(df_extdate_3,"./data/t2m_DJF_minbyrange.txt",sep="\t",row.names=FALSE)
+# write.table(df_extdate_3,"./data/t2m_DJF_minbyrange.txt",sep="\t",row.names=FALSE)
 #multiplot t2m et tp
 # p_tp <- plot_submean(df_extdate_3,xlabel = "Date",ylabel = var_label,legend_title = "Time range\n(days)")
 # p_t2m <- plot_submean(df_extdate_3,xlabel = "Date",ylabel = var_label,legend_title = "Time range\n(days)")
-multiplot(p_tp, p_t2m,cols=1)
+# multiplot(p_tp, p_t2m,cols=1)
 
 ### Sur le période 2000-2021 ###
 df_extdate_XXI <- df_extdate[df_extdate$date>1999,]
 df_extdate_XXI_3 <- select_extremes(df_extdate_XXI,var,n_days,extreme,3)
 df_extdate_XXI_3 <- df_extdate_XXI_3[c(10:12,1:3,4:6,7:9),]   #change l'odre des lignes pour que l'affichage se fasse dans le bon ordre lors du plot
+df_extdate_XXI_3$date <- as.numeric(df_extdate_XXI_3$date)
 df_extdate_XXI_3$n_days <- factor(df_extdate_XXI_3$n_days, levels=c(90,30,10,3))    #change l'ordre des facteurs pour que la légende s'affiche dans le bon ordre
 
 #plot
